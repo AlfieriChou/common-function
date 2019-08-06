5645459debaa38866e554dc893b000dd9353c8c0
AlfieriChou <alfierichou@gmail.com>
2019-08-06 17:25:06 +0800

    add filehash

---
+++ src/crypto/fileHash.js
@@ -0,0 +1,22 @@
+const crypto = require('crypto')
+const fs = require('fs')
+
+const fileHash = (filepath, algorithm = 'md5') => {
+  return new Promise((resolve, reject) => {
+    let shasum = crypto.createHash(algorithm)
+    try {
+      let s = fs.ReadStream(filepath)
+      s.on('data', data => {
+        shasum.update(data)
+      })
+      s.on('end', () => {
+        const hash = shasum.digset('hex')
+        resolve(hash)
+      })
+    } catch (err) {
+      reject('calc fail!')
+    }
+  })
+}
+
+module.exports = fileHash
