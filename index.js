7d399c1ffd198e6d7768f01584f126986a4ec55b
AlfieriChou <alfierichou@gmail.com>
2019-08-07 20:05:44 +0800

    hign func

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const { getCookie } = require('./src/axios/cookie')
-const { sendFormData } = require('./src/axios/form')
+const getCookie = require('./src/axios/cookie')
+const sendFormData = require('./src/axios/form')
+const resize = require('./src/video/resize')
+const fileHash = require('./src/crypto/fileHash')
+const { encrypted, decrypted } = require('./src/crypto')
+const sleep = require('./src/sleep')
+const CommonRedis = require('./src/cache/redisThrottle')
+const objThrottle = require('./src/cache/objThrottle')
 
 module.exports = {
   getCookie,
-  sendFormData
+  sendFormData,
+  resize,
+  fileHash,
+  encrypted,
+  decrypted,
+  sleep,
+  objThrottle,
+  CommonRedis
 }
