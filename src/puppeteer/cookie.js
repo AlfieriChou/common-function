fe03043d450d172e1d84500b36ff2eb036ab1e75
AlfieriChou <alfierichou@gmail.com>
2019-08-13 09:24:29 +0800

    puppeteer get cookies

---
+++ src/puppeteer/cookie.js
@@ -0,0 +1,29 @@
+export const setCookies = async (page, cookies) => {
+  const items = cookies
+    .map(cookie => {
+      const item = Object.assign({}, cookie)
+      if (!item.value) item.value = ''
+      console.assert(!item.url, `Cookies must have a URL defined`)
+      console.assert(
+        item.url !== 'about:blank',
+        `Blank page can not have cookie '${item.name}'`
+      )
+      console.assert(
+        !String.prototype.startsWith.call(item.url || '', 'data:'),
+        `Data URL page can not have cookie '${item.name}'`
+      )
+      return item
+    })
+    .filter(cookie => cookie.name)
+
+  await page.deleteCookie(...items)
+
+  if (items.length)
+    await page._client.send('Network.setCookies', { cookies: items })
+}
+
+export const getCookies = async page => {
+  const { cookies } = await page._client.send('Network.getAllCookies', {})
+
+  return cookies
+}
